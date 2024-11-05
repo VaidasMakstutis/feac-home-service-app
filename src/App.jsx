7920bb2a37dd4858238dddf656bd7597a7251099
Vaidas <vaidas.makstutis@gmail.com>
2024-11-05 22:04:40 +0200

    Separate router file

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,6 @@
-import { Layout } from "./components/Layout/Layout";
-import { AuthLayout } from "./components/Layout/AuthLayout";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { ROUTES } from "./router/consts";
-import { Home } from "./pages/Home";
-import { AboutUs } from "./pages/AboutUs";
-import { Services } from "./pages/Services";
-import { ErrorPage } from "./pages/ErrorPage";
-import { Login } from "./pages/Login";
-import { Register } from "./pages/Register";
-import { SearchCategory } from "./pages/SearchCategory";
+import { RouterProvider } from "react-router-dom";
 import { UserProvider } from "./contexts/UserContext";
-
-const router = createBrowserRouter([
-  {
-    element: <Layout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: ROUTES.HOME,
-        element: <Home />
-      },
-      {
-        path: ROUTES.SERVICES,
-        element: <Services />
-      },
-      {
-        path: ROUTES.ABOUT_US,
-        element: <AboutUs />
-      },
-      {
-        path: ROUTES.SEARCH_CATEGORY,
-        element: <SearchCategory />
-      }
-    ]
-  },
-  {
-    element: <AuthLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: ROUTES.LOGIN,
-        element: <Login />
-      },
-      {
-        path: ROUTES.REGISTER,
-        element: <Register />
-      }
-    ]
-  }
-]);
+import { router } from "./router/router";
 
 function App() {
   return (
