8c78d97f73fe36e9fcd70a680ecd8213b5d7c16d
Vaidas <vaidas.makstutis@gmail.com>
2024-11-11 16:43:46 +0200

    User model and validation schema

---
+++ server/users/validate.js
@@ -0,0 +1,12 @@
+const Joi = require("joi");
+
+const userSchema = Joi.object({
+  name: Joi.string().required(),
+  age: Joi.number(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required().min(8)
+});
+
+module.exports = {
+  userSchema
+};
