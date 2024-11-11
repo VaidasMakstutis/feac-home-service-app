86dab940e737c54ec9eca36c7d5114890ecb9f54
Vaidas <vaidas.makstutis@gmail.com>
2024-11-11 16:44:07 +0200

    User login and register

---
+++ server/users/login.js
@@ -0,0 +1,31 @@
+const { userModel } = require("./userModel");
+const { generateToken } = require("./password");
+
+async function login(req, res) {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "Please provide email and password" });
+    }
+
+    const findUser = await userModel.findOne({ email });
+    if (!findUser) {
+      return res.status(401).json({ message: "Incorrect email or password" });
+    }
+
+    const passwordIsCorrect = await findUser.isCorrectPassword(password);
+    if (!passwordIsCorrect) {
+      return res.status(401).json({ message: "Incorrect email or password" });
+    }
+
+    const token = generateToken({ id: findUser._id });
+
+    return res.status(200).json({ status: "success", token, user: findUser });
+  } catch (error) {
+    return res.status(500).json({ message: "Error logging in.", error: error.message });
+  }
+}
+
+module.exports = {
+  login
+};
---
+++ server/users/register.js
@@ -0,0 +1,37 @@
+const { userModel } = require("./userModel");
+const { userSchema } = require("./validate");
+
+async function register(req, res) {
+  const userData = req.body;
+
+  // Joi validation
+  const { error } = userSchema.validate(userData);
+  if (error) {
+    return res.status(400).json({
+      error: "Validation failed",
+      message: error.details.map(e => e.message)
+    });
+  }
+
+  // TODO: patikrinti ar duomenys teisingi, pvz slaptažodis reikiamo formato
+  const findUser = await userModel.findOne({ email: userData.email });
+  if (findUser) {
+    return res.status(400).json({ message: "User with this email already exists" });
+  }
+
+  try {
+    const createdUser = userModel(userData);
+    await createdUser.save();
+
+    return res.status(201).json({
+      message: "User registered successfully",
+      userData: createdUser
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Error registering new user.", error: error.message });
+  }
+}
+
+module.exports = {
+  register
+};
