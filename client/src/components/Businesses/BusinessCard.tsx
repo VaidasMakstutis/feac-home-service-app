151b50962808369c2445f5095414eea7c7aaf499
Vaidas <vaidas.makstutis@gmail.com>
2024-11-30 20:50:33 +0200

    Included email and description to card component

--- client/src/components/Businesses/BusinessCard.tsx
+++ client/src/components/Businesses/BusinessCard.tsx
@@ -12,7 +12,9 @@ export function BusinessCard({ business }: BusinessCardProps) {
       <div className={styles.cardInfo}>
         <p className={styles.category}>{business.category}</p>
         <h4 className={styles.name}>{business.name}</h4>
+        <p className={styles.description}>{business.description}</p>
         <p className={styles.person}>{business.contactPerson}</p>
+        <p className={styles.email}>{business.email}</p>
         <p className={styles.address}>{business.address}</p>
         <button className={styles.button}>Book now</button>
       </div>
